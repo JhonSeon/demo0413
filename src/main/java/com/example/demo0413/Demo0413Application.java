a7d136b6186e1bbede5f65d88a1f9d139e967349
511411352@qq.com <511411352@qq.com>
2019-04-13 15:56:21 +0800

    111

--- src/main/java/com/example/demo0413/Demo0413Application.java
+++ src/main/java/com/example/demo0413/Demo0413Application.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo0413Application {
 
     public static void main(String[] args) {
-         System.out.println();
         SpringApplication.run(Demo0413Application.class, args);
     }
 
