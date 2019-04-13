860a4f6bf8e674ccc515877084f1f1fc82f68197
JhonSeon <511411352@qq.com>
2019-04-13 15:55:40 +0800

    Update Demo0413Application.java

--- src/main/java/com/example/demo0413/Demo0413Application.java
+++ src/main/java/com/example/demo0413/Demo0413Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Demo0413Application {
 
     public static void main(String[] args) {
+         System.out.println();
         SpringApplication.run(Demo0413Application.class, args);
     }
 
